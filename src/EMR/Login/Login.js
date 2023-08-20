b8c632d111befd3d367f9857855874e6db9f3f2a
Thien <vanthiencntt@gmail.com>
2023-08-20 12:53:38 +0700

    add

---
+++ src/EMR/Login/Login.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Login.css";
+
+const Login = () => {
+    return (
+        <>
+            <header>
+                <h2>Helo</h2>
+            </header>
+        </>
+    )
+}
+export default Login
