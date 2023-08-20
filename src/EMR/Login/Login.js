3efec94fc307a497c62c53a0f1001149958c17ac
Thien <vanthiencntt@gmail.com>
2023-08-20 21:23:13 +0700

    add from login call api

--- src/EMR/Login/Login.js
+++ src/EMR/Login/Login.js
@@ -1,60 +1,185 @@
-import React from "react";
-import { useState, useEffect } from "react";
-import axios from "axios";
+import React, { useState, useEffect } from "react";
+import * as ServerApi from "../../ServerAPI/ServerApi";
 import "./Login.css";
 
-
-import * as ServerApi from "../../ServerAPI/ServerApi";
-
 const Login = () => {
+    const hashpasss = {
+        password: ''
 
+    }
+    const reders = {
+        fullname: ''
+    };
     const [doituongs, setDoituongs] = useState([]);
+    //   const [hashpass, setHashpass] = useState("");
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [render, setRender] = useState([]);
 
     useEffect(() => {
         ServerApi.get('/danhmuc/getdoituong')
             .then(function (response) {
                 // handle success
                 setDoituongs(response);
-                //console.log(response);
-                // console.log(response.data); // Make sure the data you need is in response.data
-                //  setDoituongs(response.data); // Update the component state with the fetched data
             })
             .catch(function (error) {
                 // handle error
-                // console.log(error);
-            })
-            .finally(function () {
-                // always executed
+                console.log("Error:", error);
             });
     }, []);
-    //  console.log(doituongs);
+
+    const onChangeUsername = (event) => {
+        setUsername(event.target.value);
+    };
+
+    const onChangePassword = (event) => {
+        setPassword(event.target.value);
+    };
+
+
+    const onClick = (event) => {
+
+
+        ServerApi.post('/EmrUseridLogin/hash-password?password=' + password)
+            .then(function (response) {
+
+                //   setHashpass(response); // Set the hashed password in the state
+                hashpasss.password = response;
+
+                console.log(hashpasss.password);
+                ServerApi.post('/EmrUseridLogin/GetEmrUseridLogin', {
+                    "userid": username,
+                    "password": hashpasss.password
+                }
+                )
+                    .then(function (response) {
+                        reders.fullname = response.fullname;
+
+                        console.log("Response", reders.fullname);
+                        //  console.log("Response status:", response.status);
+                        if (response.status = 200) {
+                            alert("Đăng nhập thành công ");
+                        }
+
+                        else {
+                            alert("Vui lòng nhập lại ");
+                        }
+                    })
+                    .catch(function (error) {
+                        // handle error
+                        console.log("Error:", error);
+                    });
+                return reders.fullname;
+
+
+            })
+            .catch(function (error) {
+                // handle error
+                console.log("Error:", error);
+            });
+
+
+    };
+
+    // useEffect(() => {
+    //     console.log("Response", reders.fullname);
+    //     setRender(reders.fullname);
+    //     console.log(render)
+    // }, []);
 
     return (
         <>
-            <header>
+            {/* <header>
                 <h1 className="id">Doituong:</h1>
                 <ul>
-                    {
-                        doituongs.map((doituong) => {
-                            return (
-                                <div>
-                                    <table className="table">
-                                        <tr>
-                                            <td> {doituong.madoituong}</td>
-                                            <td>{doituong.doituong}</td>
-                                        </tr>
-
-                                    </table>
-                                </div>
-
-                            )
-                        }
-
-                        )
-                    }
+                    {doituongs.map((doituong) => (
+                        <div key={doituong.madoituong}>
+                            <table className="table">
+                                <tr>
+                                    <td>{doituong.madoituong}</td>
+                                    <td>{doituong.doituong}</td>
+                                </tr>
+                            </table>
+                        </div>
+                    ))}
                 </ul>
-            </header>
+            </header> */}
+
+
+            <div class="box-form">
+                <div class="left">
+                    <div class="overlay">
+                        <h1>Hello World.</h1>
+
+                        <span>
+                            <p>login with social media</p>
+                            <a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                            <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i> Login with Twitter</a>
+                        </span>
+                    </div>
+                </div>
+                <div class="right">
+
+                    <h5>Login</h5>
+                    <div class="inputs">
+                        <input type="text" onChange={onChangeUsername}
+                            value={username} placeholder="user name" />
+                        <input type="password" onChange={onChangePassword}
+                            value={password} placeholder="password" />
+                    </div>
+                    <div class="remember-me--forget-password">
+                        <label>
+                            <input type="checkbox" name="item" checked />
+                            <span class="text-checkbox">Lưu Password</span>
+                        </label>
+                        <p>forget password?</p>
+                    </div>
+                    <button onClick={onClick}>Login</button>
+                </div>
+            </div>
+
+            {/* <form>
+                <div>Username</div>
+                <input
+                    type="text"
+                    name="username"
+                    onChange={onChangeUsername}
+                    value={username}
+                />
+                <div>Password</div>
+                <input
+                    type="password" // Use type="password" for password input
+                    name="password"
+                    onChange={onChangePassword}
+                    value={password}
+                />
+                <div>
+                    <input
+                        type="button" // Use type="button" for the submit button
+                        name="submit"
+                        value="Submit"
+                        onClick={onClick}
+                    />
+                </div>
+            </form> */}
+            <>
+                {/* <div>
+                    <ul>
+                        {render.map((rd) => (
+                            <div key={rd.fullname}>
+                                <table className="table">
+                                    <tr>
+                                        <td>{rd.fullname}</td>
+
+                                    </tr>
+                                </table>
+                            </div>
+                        ))}
+                    </ul>
+                </div> */}
+            </>
         </>
-    )
-}
-export default Login
+    );
+};
+
+export default Login;
--- src/ServerAPI/ServerApi.js
+++ src/ServerAPI/ServerApi.js
@@ -8,5 +8,12 @@ export const get = async (part, op = {}) => {
     const response = await ServerApi.get(part, op); // Use ServerApi.get instead of request.get
     return response.data;
 }
+export const post = async (part, op = {}) => {
+    const response = await ServerApi.post(part, op); // Use ServerApi.get instead of request.get
+
+    return response.data;
+}
+
+
 
 export default ServerApi;
